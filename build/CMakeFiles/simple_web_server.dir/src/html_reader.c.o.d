c81810fd49904fa4650d8b5f3fe17ec5c7594c9d
risc <43586571+aurzenith@users.noreply.github.com>
2025-07-29 09:28:52 -0400

    Delete build directory

--- build/CMakeFiles/simple_web_server.dir/src/html_reader.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/simple_web_server.dir/src/html_reader.c.o: \
- /home/risc/source/A-Simple-Web-Server/src/html_reader.c \
- /usr/include/stdc-predef.h \
- /home/risc/source/A-Simple-Web-Server/src/html_reader.h
