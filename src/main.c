98b6bbe9701d4e52d6545eb7ae5b6e1aa1754ef4
risc <aurzenith@gmail.com>
2025-10-10 19:01:07 -0400

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -1,84 +1,80 @@
 /* Kris 2025-07-25*/
 
-#include "web_server.h"
 #include "html_reader.h"
+#include "web_server.h"
 
+int main(int argc, char **argv) {
+  int errorValue;
+  FILE *html_fptr;
+  char *html_file = "../index.html";
+  struct addrinfo hints, *servInfo;
+  char buff[2048];
+  char *http_response;
 
-int main(int argc, char** argv) {
-    int errorValue;
-    FILE* html_fptr;
-    char* html_file = "../index.html";
-    struct addrinfo hints, *servInfo;
-    char buff[2048];
-    char* http_response;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
 
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM; 
+  html_fptr = fopen(html_file, "r");
+  if (html_file == NULL) {
+    printf("Error opening file");
+    return 1;
+  }
 
-    html_fptr = fopen(html_file, "r");
-    if (html_file == NULL) {
-       printf("Error opening file");
-       return 1;
-    }
+  char *html_content = readHTMLFile(html_fptr);
+  if (html_content == NULL) {
+    printf("Error reading file");
+    return 1;
+  }
 
-    char* html_content = readHTMLFile(html_fptr);
-    if (html_content == NULL) {
-        printf("Error reading file");
-        return 1;
-    }
+  printf("%s", html_content);
 
-    printf("%s", html_content);
+  // manually inputted for now,
+  char *ipAddr = "127.0.0.1";
+  struct sockaddr_storage client_addr;
+  socklen_t addr_size;
+  int servsockfd, clientsockfd;
 
-    
-    // manually inputted for now,
-    char* ipAddr = "127.0.0.1";
-    struct sockaddr_storage client_addr;
-    socklen_t addr_size;
-    int servsockfd, clientsockfd;
+  char *port = "8080";
 
+  // set up addressing in its struct
+  if ((errorValue = getaddrinfo(ipAddr, port, &hints, &servInfo)) != 0) {
+    fprintf(stderr, "getaddinfo: %s\n", gai_strerror(errorValue));
+    return 1;
+  }
 
-    char* port = "8080";
+  printAddressesInfo(servInfo);
 
+  servsockfd =
+      socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
 
-    // set up addressing in its struct
-    if ((errorValue = getaddrinfo(ipAddr, port, &hints, &servInfo)) != 0) {
-        fprintf(stderr, "getaddinfo: %s\n", gai_strerror(errorValue));
-        return 1;
-    }
+  errorValue = bind(servsockfd, servInfo->ai_addr, servInfo->ai_addrlen);
+  listen(servsockfd, 10);
 
-    printAddressesInfo(servInfo);
+  addr_size = sizeof(client_addr);
 
-    servsockfd = socket(servInfo->ai_family, servInfo->ai_socktype, servInfo->ai_protocol);
+  http_response =
+      createHtmlResponse(HTTP_200_OK, html_content, strlen(html_content));
+  if (http_response == NULL) {
+    fprintf(stderr, "Failed to create HTTP response. Exiting.\n");
+    return 4;
+  }
 
-    bind(servsockfd, servInfo->ai_addr, servInfo->ai_addrlen);
-    listen(servsockfd, 10);
+  // keep server open for multiple requests
+  while (1) {
+    clientsockfd =
+        accept(servsockfd, (struct sockaddr *)&client_addr, &addr_size);
+    recv(clientsockfd, buff, 2048, 0);
+    printf("%s", buff);
 
-    addr_size = sizeof(client_addr);
+    send(clientsockfd, http_response, strlen(http_response), 0);
 
-    http_response = createHtmlResponse(HTTP_200_OK, html_content, strlen(html_content)); 
-    if (http_response == NULL) {
-        fprintf(stderr, "Failed to create HTTP response. Exiting.\n");
-        return 4;
-    }
+    // logging will create actual logging file stuff later...
+  }
 
-    // keep server open for multiple requests
-    while(1) {
-        clientsockfd = accept(servsockfd, (struct sockaddr*) &client_addr, &addr_size);
-        recv(clientsockfd, buff, 2048, 0);
-        printf("%s", buff);
-        
-        send(clientsockfd, http_response, strlen(http_response), 0);
-
-
-        // logging will create actual logging file stuff later...
-  
-    }
-        
-
-    free(http_response);
-    fclose(html_fptr);
-    free(html_content);
-    freeaddrinfo(servInfo);
-    return 0;
+  free(http_response);
+  fclose(html_fptr);
+  free(html_content);
+  freeaddrinfo(servInfo);
+  return 0;
 }
